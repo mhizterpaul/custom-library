0423d34c693b36282df63b0f0fca4097c5ae2c11
Paul <chinonsopaul@gmail.com>
2018-10-28 00:13:57 +0100

    initial commit

---
+++ Library.js
@@ -0,0 +1,185 @@
+'use strict';
+ /*
+ * custom library by paul chinonso
+ * Licence: MIT
+ */
+
+(function(){
+
+//Templating Engine
+const templatingEngine = (string, startDelim = '|', endDelim) => {
+	//check to make sure that the arguments are strings
+	if(typeof(string) !== 'string' || typeof(startDelim) !== 'string' || typeof(endDelim) !== 'string' && endDelim){
+		console.log('please enter a string with either the default delimeters or any custom delimeters \
+			and pass delimeter strings as arguments');
+		return;
+	}
+
+	//initialize variables
+	const matches = [], stringsToInterpolate = [], endMatches = [];
+	let startIndex = 0, newString = '`';
+
+	//loop through string and retrieve the index number of starting delimeters
+	//and ending delimeters iff both deleimeters are the same
+	for(let i = 0; i <= string.length-1; i++ ){
+		const match = string.indexOf(startDelim, i);
+		if(match === -1 ) break;
+		matches.push(match);
+		i = match + 1;
+	}
+
+	//if no match was found return the string in a new function
+	if(matches.length === 0) return new Function(`num`, `for(let i = 1; i <= num; i++ )
+	{
+		console.log('${string}');
+	}
+	return '${string}';`);
+
+	//takes the index number of end delimeters if any
+	if(endDelim && endDelim !== startDelim){
+		for(let i = 0; i <= matches.length-1; i++ ){
+
+			const match = string.indexOf(endDelim, matches[i] + 1);
+			if(match === -1 ) break;
+			endMatches.push(match);
+		}
+	}
+
+	//uses delimeter's index to retrieve text content for variables and arguments
+	for(let i = 0; i <= matches.length-1; i++ ){
+
+		if(!endDelim || endDelim === startDelim){
+			let customString = string.slice(matches[i] + startDelim.length, matches[i+1]);
+			//take the string before the start delimeter and between similar delimeter
+			newString += string.slice(startIndex, matches[i]) + "${" + customString + "}";
+			//check the custom string for any . and remove any if found
+			if(customString.indexOf('.') > -1)	customString = customString.slice(0, customString.indexOf('.'));
+			//trim custom string for use as an argument
+			stringsToInterpolate.push(customString.trim());
+			startIndex = matches[i+1] + startDelim.length;
+			i += 1;
+
+		}else{
+
+			let customString = string.slice(matches[i] + startDelim.length, endMatches[i]);
+			//take the string before the start delimeter and between disimilar delimeter
+			newString += string.slice(startIndex, matches[i]) + "${" + customString + "}";
+			//check the custom string for any . and remove any if found
+			if(customString.indexOf('.') > -1)	customString = customString.slice(0, customString.indexOf('.'));
+			//trim custom string for use as an argument
+			stringsToInterpolate.push(customString.trim());
+			startIndex = endMatches[i] + endDelim.length;
+
+		}
+		//when the final delimeter has been reached grab all the
+		//remaining string and add it to newString
+		if(i === matches.length-1) newString += string.slice(startIndex) + '`';
+		
+	}
+
+	//remove any repeated strings in the list for argument
+	for(let i = stringsToInterpolate.length - 1; i > 0; i--){
+
+		if(stringsToInterpolate[i] === stringsToInterpolate[i-1]) stringsToInterpolate.pop();			
+
+	 }
+
+	 //return new function ready to be called with data for interpolation;
+	return new Function(...stringsToInterpolate, `num`, 
+	`for(let i = 1; i <= num; i++ ){
+
+		console.log(${newString});
+	}
+	return ${newString};`);
+}
+
+//Event Management System
+ class EventTracker {
+ 	constructor(name){
+ 		this.name= name;
+ 	}
+
+ 	test(arg1, arg2){
+		if(typeof(arg1) === 'function' || typeof(arg1) === 'object' && arg1 instanceof EventTracker){
+			return typeof(arg1) === 'function' ? 'function' : 'object';
+ 		}else if(arg1 instanceof Array){
+
+ 			//filter the array and return only callbacks or objects;
+ 			if(arg2 === 'function'){
+ 				const newArray = arg1.filter((func) => typeof(func) === 'function');
+ 				//check the new array if empty return undefined if not return the array
+ 				return newArray.length > 0 ? newArray : undefined;
+ 			}
+ 			if(arg2 === 'object'){
+ 				const newArray = arg1.filter((obj) => typeof(obj) === 'object' && obj instanceof EventTracker);
+ 				return newArray.length > 0 ? newArray : undefined;
+ 			}
+ 		}else{
+ 			return undefined;
+ 		}
+ 	}
+
+ 	on(event, callback){
+ 		//add an event property to the object if there's none, property should point to an object
+ 		//add key, pair values for event name and array of callbacks
+ 		if( !this.test(callback, 'function') || typeof(event) !== 'string') return;
+		const update = () => {
+	 		if(this.test(callback, 'function') === 'function'){
+	 				this._events[event].push(callback);
+	 		}else{
+	 			this.test(callback, 'function').forEach((func) => this._events[event].push(func));
+	 		}
+ 		};
+ 		const create = () => {
+ 			this._events[event] = [];
+ 			update();
+ 		};
+ 		const setUp = () => {
+ 			this._events = {};
+ 			create();
+ 		};
+ 		this._events ? this._events[event] ? update() : create() : setUp();
+ 		
+ 	}
+
+ 	notify(obj, event){
+ 		//check if ._notify is defined if not create it;
+ 		//then add event name and object to notify, as key pair value;
+ 		if( !this.test(obj, 'object') || typeof(event) !== 'string') return;
+ 		const update = () => {
+ 			if(this.test(obj, 'object') === 'object'){
+ 				this._notify[event].push(obj);
+ 			}else{
+ 				this.test(obj, 'object').forEach((object) => this._notify[event].push(object));
+ 			}
+ 			
+ 		}; 
+ 		const create = () => {
+ 			this._notify[event] = [];
+ 			update();
+ 		};
+ 		const setUp = () => {
+ 			this._notify = {};
+ 			create();
+ 		};
+ 		this._notify ?  this.notify[event] ? update() : create() : setUp();
+ 		
+ 	}
+
+ 	trigger(event, ...data){
+ 		//loops through the event and call the functions attached to that event
+ 		//check the notify property for any object to be notified of this event
+ 		//notify them;
+ 		if(typeof(event) !== 'string' || !this._events || !this._events[event]) return;
+ 		this._events[event].forEach((callback) => callback(...data));
+ 		if(this._notify && this._notify[event]) this._notify[event].forEach((obj) => obj.trigger(event));
+ 	}
+ }
+
+ window.library = {
+ 	templatingEngine : templatingEngine,
+ 	eventTracker : EventTracker
+ 
+ }
+
+})();
---
+++ README.md
@@ -0,0 +1,49 @@
+# Library Of Custom Functionalites
+
+## Table of Contents
+
+* [About](#About)
+* [Usage](#Usage)
+* [Contributing](#contributing)
+* [License](#License)
+
+
+## About
+
+This repo contains a library that provides custom functionalities. Most part of the applications are my answers to the udacity's Front End Frameworks Course. the name of each functionality is quite descriptive of what it does.
+
+## Usage 
+
+The application provides a 'library' Interface which provides the custom functionalities. It's currently has two methods, i. templatingEngine ii. eventTracker.
+
+* templatingEngine: 
+
+ To use this functionality you have to call library's templating engine with two parameters if the starting and ending delimeters are the same string or three parameters if the starting and ending delimeters are dissimilar. The first parameter is the string you want to use as a template, string must contain starting and ending delimeters for variable content(s) or the string will be returned, the string must also not contain any starting and ending delimeters in it's static body, due to how to engine works. The templating engine returns a function which you can call with data and an optional number that indicate how many times it should be logged to the console.
+ Example: library.templatingEngine('my name is |name|, I am |height|meters tall', '|')('paul', 152, 2); 
+
+ library.templatingEngine(`<td><a href="#detail/<%= menuItem.id %>"><%= menuItem.name %></a></td>
+    <td><%= menuItem.rating %></td>
+    <td><%= menuItem.calories %></td>
+    <td>
+      <button class="select-item">Select Item</button>
+    </td>`,'<%=', '%>')({id: 1, name: 'paul', rating: 2, calories: 5}, 5);
+
+* eventTracker
+ this is a constructor function that return an object with on, notify and trigger methods. 
+  ** the on method
+     It is used to add a custom events to the eventTracker instance
+     it takes a name parameter and a function or array of functions.
+  ** the notify method
+     It takes two parameters, an eventTracker instance and an event you want the object to be notified of.
+  ** the trigger method
+     It does what it sounds like!.. triggering an event. It takes two parameters, i. name of custom event ii. data for the custom event.
+
+
+## Contributing
+
+Most of my codes are sloppy and maybe require a little more organisation. If you feel there are improvements to make. Please fork this repo, create and new branch, make the required changes and submit a pull request.
+
+
+## License
+
+[MIT](https://opensource.or/licenses/mit-license.php).
